79b9d3dc373c5e7674a2263702288c26209f7154
drson13773 <ngocson89.db@gmail.com>
2026-09-15 00:01:42 +0700

    Update NotificationCenterOverlay.kt

--- app/src/main/java/com/drson/launcher/ui/NotificationCenterOverlay.kt
+++ app/src/main/java/com/drson/launcher/ui/NotificationCenterOverlay.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
