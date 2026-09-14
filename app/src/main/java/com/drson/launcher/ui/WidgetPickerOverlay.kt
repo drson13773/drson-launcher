3f380a2bb3d0351268b8efebaccb06316a7bdc38
drson13773 <ngocson89.db@gmail.com>
2026-09-15 00:02:51 +0700

    Update WidgetPickerOverlay.kt

--- app/src/main/java/com/drson/launcher/ui/WidgetPickerOverlay.kt
+++ app/src/main/java/com/drson/launcher/ui/WidgetPickerOverlay.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
